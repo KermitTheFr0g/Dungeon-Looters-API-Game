9cb5c5044c9cea62e7446730381c5ff1d845e508
Kermit2626 <oligray26@gmail.com>
2023-05-11 22:30:30 +0100

    middleware

--- middleware/verifyToken.ts
+++ middleware/verifyToken.ts
@@ -5,18 +5,22 @@ import prisma from '../prisma/prisma';
 async function verifyTokenIncluded(req: Request, res: Response, next: NextFunction) {
     const apiToken = req.query.token;
     
+    // * checks if api token is included in the request
+    // * returns error if not included
     if(!apiToken){
         return res.status(400).json({
             error: 'API token is required'
         })
     }
 
+    // * checks if api token is valid
     const getUser = await prisma.user.findFirst({
         where: {
             api_token: apiToken as string
         }
     })
 
+    // * return error if api token is invalid
     if(!getUser){
         return res.status(400).json({
             error: 'Invalid API token'
